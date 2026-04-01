c4920c8c98afb63449ca5f323e70428f2cbc40bc
natan <natanael0junior@gmail.com>
2026-03-31 21:26:09 -0300

    add frontend inital structure and setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <section id="center">
+        <div className="hero">Bom dia, comunidade!</div>
+      </section>
+    </>
+  );
+};
+export default App;
